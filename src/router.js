2229c70c2a04117580025aa2ab2f88c2606a65b2
Lauryn <laurynpearson@gmail.com>
2019-08-06 20:33:48 -0500

    create show page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import ShowBook from './views/ShowBook.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/books/:id',
+      name: 'books',
+      component: ShowBook
     },
     {
       path: '/about',
---
+++ src/views/ShowBook.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="show">
+    <h1>{{ message }}</h1>
+    <p>title: {{ book.title }}</p>
+    <p>pages: {{ book.pages }}</p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+  data: function() {
+    return {
+      message: "Welcome to the show!",
+      book: {}
+    };
+  },
+  created: function() {
+    axios.get(`/api/books/${this.$route.params.id}`).then(response => {
+      console.log(response.data);
+      this.book = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
